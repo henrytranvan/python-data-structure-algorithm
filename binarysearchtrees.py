f0941d78aa2a52e5939cfdf519672519a346c675
henry <hentran@deloitte.com.au>
2023-09-06 12:19:16 +1000

    Update binary search tree data structure.

--- binarysearchtrees.py
+++ binarysearchtrees.py
@@ -17,15 +17,17 @@ class BinarySearchTrees:
         current_node = self.root
         while True:
             if current_node.value == val:
-                return
+                return False
             if current_node.value > val:
                 if current_node.left is None:
                     current_node.left = new_node
+                    return True
                 else:
                     current_node = current_node.left
             else:
                 if current_node.right is None:
                     current_node.right = new_node
+                    return True
                 else:
                     current_node = current_node.right
 
@@ -33,4 +35,9 @@ class BinarySearchTrees:
 my_tree = BinarySearchTrees()
 my_tree.insert(50)
 my_tree.insert(40)
-print(my_tree.root.left.value)
+my_tree.insert(30)
+my_tree.insert(60)
+my_tree.insert(70)
+my_tree.insert(80)
+
+print(my_tree.root.right.right.value)
