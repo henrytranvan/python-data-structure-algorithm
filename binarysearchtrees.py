37c81992161c6913a9fb91f51ccad2bc5c190a54
henry <hentran@deloitte.com.au>
2023-09-06 16:26:10 +1000

    Update binary search tree data structure.

--- binarysearchtrees.py
+++ binarysearchtrees.py
@@ -4,6 +4,7 @@ class Node:
         self.left = None
         self.right = None
 
+
 class BinarySearchTrees:
     def __init__(self):
         self.root = None
@@ -43,6 +44,39 @@ class BinarySearchTrees:
 
         return False
 
+    def __r_contain(self, current_node, val):
+        if current_node is None:
+            return False
+        if current_node.value == val:
+            return True
+        if val < current_node.value:
+            return self.__r_contain(current_node.left, val)
+        else:
+            return self.__r_contain(current_node.right, val)
+
+    def r_contain(self, val):
+        return self.__r_contain(self.root, val)
+
+    def __r_insert(self, current_node, val):
+        if current_node is None:
+            return
+        if current_node.value == val:
+            return
+        if val < current_node.value:
+            if current_node.left is None:
+                current_node.left = current_node
+            else:
+                self.__r_insert(current_node.left, val)
+        else:
+            if current_node.right is None:
+                current_node.right = current_node
+            else:
+                self.__r_insert(current_node.right, val)
+
+    def r_insert(self, val):
+        return self.__r_insert(self.root, val)
+
+
 my_tree = BinarySearchTrees()
 my_tree.insert(50)
 my_tree.insert(40)
@@ -62,3 +96,33 @@ if my_tree.contain(90):
     print("90 is in the tree")
 else:
     print("90 is not in the tree")
+
+if my_tree.r_contain(80):
+    print("80 is in the tree")
+else:
+    print("80 is not in the tree")
+
+if my_tree.r_contain(90):
+    print("90 is in the tree")
+else:
+    print("90 is not in the tree")
+
+
+my_tree = BinarySearchTrees()
+my_tree.r_insert(55)
+my_tree.insert(45)
+my_tree.insert(35)
+my_tree.insert(65)
+my_tree.insert(75)
+my_tree.insert(85)
+
+
+if my_tree.r_contain(85):
+    print("85 is in the tree")
+else:
+    print("85 is not in the tree")
+
+if my_tree.r_contain(95):
+    print("95 is in the tree")
+else:
+    print("95 is not in the tree")
